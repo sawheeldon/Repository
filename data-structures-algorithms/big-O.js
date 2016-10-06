93544835abb16bdce78696435d7a1086ed2baccb
sawheeldon <simon@jampot.design>
2016-10-06 02:44:35 +0100

    update

--- data-structures-algorithms/big-O.js
+++ data-structures-algorithms/big-O.js
@@ -29,3 +29,55 @@ var sum = function(array) {
 //O(n^2) - Polynomial time. As n increases, things start to get slow pretty quickly.
 //O(2^n) - Exponential time. As n increases, things start to get slow incredibly quickly.
 //O(n!) - Factorial time. Everything bad about exponential time. But worse.
+
+
+var App = React.createClass({
+    getInitialState: function() {
+        return {
+            showResults: false,
+            articles: [ ]
+        };
+
+    },
+
+    constructor: function() {
+        super();
+        this.state = {
+                        showResults: false,
+,
+            articles: [showResults: false,]
+        }
+    }
+    componentDidMount: function(){
+       fetch('https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=9e6d16842a6b4368b4937a31ccf54035')
+            .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        this.setState({ articles: [{id: 1, name:'Ray'}, {id:2, text:"AAAAAA"}]});
+    });
+    },
+    onClick: function() {
+        this.setState({ showResults: true });
+        $('.titlePage').hide();
+        $('.submitButton').hide();
+    },
+
+    render: function() {
+        return (
+            <div>
+                <div> <TitlePage/> </div>
+                <div className="col-xs-12">
+                <input type="submit" className="btn btn-primary submitButton" onClick={this.onClick} />
+                </div>
+                { this.state.showResults ? <MainPage /> : null }
+                {this.renderData}
+                {this.state.articles}
+            </div>
+        );
+    }
+});
